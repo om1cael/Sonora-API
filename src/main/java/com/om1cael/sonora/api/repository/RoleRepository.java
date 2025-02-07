b19ee9f937327f190d7bd3e8bffe04df69be8960
om1cael <188113439+om1cael@users.noreply.github.com>
2025-02-06 23:03:32 -0300

    feat: create user and role repository

---
+++ src/main/java/com/om1cael/sonora/api/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.om1cael.sonora.api.repository;
+
+import com.om1cael.sonora.api.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
+}
