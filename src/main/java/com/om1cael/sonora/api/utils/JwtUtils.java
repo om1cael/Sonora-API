b1fcbe99a8e53bdea4742f6e575610561e2b50e5
om1cael <188113439+om1cael@users.noreply.github.com>
2025-02-06 23:40:37 -0300

    feat: implement class for generating jwt tokens

---
+++ src/main/java/com/om1cael/sonora/api/utils/JwtUtils.java
@@ -0,0 +1,31 @@
+package com.om1cael.sonora.api.utils;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+
+@Component
+public class JwtUtils {
+
+    @Value("{jwt.secret}")
+    private String secret;
+    private final String issuer = "sonora-api";
+
+    private final int HOURS_IN_SECONDS = 3600;
+
+    public void createToken(String username) {
+        JWT.create()
+                .withSubject(username)
+                .withIssuer(issuer)
+                .withIssuedAt(Instant.now())
+                .withExpiresAt(Instant.now().plusSeconds(HOURS_IN_SECONDS * 24))
+                .sign(getAlgorithm());
+    }
+
+    private Algorithm getAlgorithm() {
+        return Algorithm.HMAC256(secret);
+    }
+}
