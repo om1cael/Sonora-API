0b3c21456cc8c34a570e1bf00e4a724a2a530457
om1cael <188113439+om1cael@users.noreply.github.com>
2025-02-06 23:50:18 -0300

    feat: create user details service implementations

---
+++ src/main/java/com/om1cael/sonora/api/service/UserDetailsServiceImpl.java
@@ -0,0 +1,22 @@
+package com.om1cael.sonora.api.service;
+
+import com.om1cael.sonora.api.model.User;
+import com.om1cael.sonora.api.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public User loadUserByUsername(String username) throws UsernameNotFoundException {
+        return this.userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
