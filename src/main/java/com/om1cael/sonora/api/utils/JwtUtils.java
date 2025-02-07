1af1407fc2263e50274c71a0d61d717efc5f79a5
om1cael <188113439+om1cael@users.noreply.github.com>
2025-02-07 00:10:38 -0300

    feat(jwt utils): make create token return the token

--- src/main/java/com/om1cael/sonora/api/utils/JwtUtils.java
+++ src/main/java/com/om1cael/sonora/api/utils/JwtUtils.java
@@ -3,7 +3,6 @@ package com.om1cael.sonora.api.utils;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.interfaces.Verification;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -18,8 +17,8 @@ public class JwtUtils {
 
     private final int HOURS_IN_SECONDS = 3600;
 
-    public void createToken(String username) {
-        JWT.create()
+    public String createToken(String username) {
+        return JWT.create()
                 .withSubject(username)
                 .withIssuer(issuer)
                 .withIssuedAt(Instant.now())
